4f00b6e456769690be535a80b551897d70041256
Arto Gahr <artogahr@gmail.com>
2026-05-06 23:53:00 +0200

    First zola test commit

---
+++ content/blog/first.md
@@ -0,0 +1,6 @@
++++
+title = "My first post using Zola"
+date = 2019-11-27
++++
+
+This is my first blog post using Zola.
---
+++ flake.nix
@@ -0,0 +1,34 @@
+# SPDX-License-Identifier: Unlicense
+{
+  inputs = {
+    # nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    # systems.url = "github:nix-systems/default";
+  };
+
+  outputs =
+    { nixpkgs, ... }@inputs:
+    let
+      eachSystem =
+        f:
+        nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed (
+          system: f system nixpkgs.legacyPackages.${system}
+        );
+    in
+    {
+      packages = eachSystem (
+        _system: pkgs: {
+          hello = pkgs.hello;
+        }
+      );
+
+      devShells = eachSystem (
+        _system: pkgs: {
+          default = pkgs.mkShell {
+            buildInputs = with pkgs; [
+              zola
+            ];
+          };
+        }
+      );
+    };
+}
